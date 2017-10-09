f3ffd6635000eebc4d171dfb2c8f37f8989a1680
Kenneth Worden <kenny.worden@gmail.com>
2017-10-09 15:31:21 -0400

    Load data and build model

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
+
 K.set_image_dim_ordering('th')
+np.set_printoptions(threshold=np.inf)
 
 def load_dataset(dataset_dir):
     # Our classifications
@@ -26,12 +28,23 @@ loaded = load_dataset('dataset')
 dataset = loaded[0]
 labels = loaded[1]
 
-print(dataset)
-print(labels)
-
 model = Sequential()
 model.add(Conv2D(10,
         kernel_size=(2, 2),
         activation='relu',
-        input_shape=dataset.shape))
+        padding='valid',
+        input_shape=(60, 10, 10)))
+model.add(MaxPooling2D(pool_size=(2, 2),
+        strides=None,
+        padding='valid',
+        data_format=None))
 model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(2, activation='relu'))
+
+model.compile(loss='categorical_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy'])
+
+model.fit(dataset, labels,
+    batch_size=5, epochs=5, verbose=1)
