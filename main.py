7e82e7cd3d8ab0e9781d3a1868c7e436141a1942
CharlieHVu <char1997@vt.edu>
2017-10-18 14:57:08 -0400

    Made various changes to model, specifically last Dense layer's activation to softmax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import keras
 from keras import losses
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -10,6 +11,9 @@ from keras import backend as K
 
 K.set_image_dim_ordering('th')
 np.set_printoptions(threshold=np.inf)
+
+batch_size = 16
+epochs = 1000
 
 def label_to_int(lbl):
     if lbl == 'line':
@@ -49,38 +53,51 @@ print('labels shape:', labels.shape)
 model = Sequential()
 model.add(Conv2D(10,
         kernel_size=(2, 2),
-        activation='tanh',
+        activation='relu',
         padding='same',
         input_shape=(10, 10, 1)))
-model.add(MaxPooling2D(pool_size=(2, 2),
+model.add(MaxPooling2D(pool_size=(1, 1),
         strides=2,
         padding='same',
         data_format=None))
-model.add(Dropout(0.25))
+model.add(Dropout(0.1))
 model.add(Conv2D(10,
         kernel_size=(2, 2),
-        activation='tanh',
+        activation='relu',
         padding='same'))
-model.add(MaxPooling2D(pool_size=(2, 2),
+model.add(MaxPooling2D(pool_size=(1, 1),
         strides=2,
         padding='same',
         data_format=None))
 model.add(Flatten())
-model.add(Dense(4, activation='relu'))
+
+model.add(Dense(4, activation='softmax'))
 
 model.summary()
-
 # Use a Stochastic-Gradient-Descent as a learning optimizer
-sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
 # Prevent kernel biases from being exactly 0 and giving nan errors
 def constrainedCrossEntropy(ytrue, ypred):
     ypred = K.clip(ypred, 1e-7, 1e7)
     return losses.categorical_crossentropy(ytrue, ypred)
-
-model.compile(loss=constrainedCrossEntropy, #'categorical_crossentropy',
-    optimizer=sgd, #'adam',
-    metrics=['accuracy'])
+model.compile(loss=constrainedCrossEntropy,
+              optimizer=sgd,
+              metrics=['accuracy'])
 
 model.fit(dataset, labels,
-    batch_size=16, epochs=1000, verbose=1)
+    batch_size=batch_size, epochs=epochs, verbose=1)
+
+score = model.evaluate(dataset, labels, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+#Saves the model
+#Serialize model to JSON
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
